d92d79f05fea65e65cba5503f17d631fb4eaacc8
ModerRAS <adqewsx@gmail.com>
2021-03-01 21:12:57 +0800

    Update Program.cs

--- Wangluodaxue2Motibang/Program.cs
+++ Wangluodaxue2Motibang/Program.cs
@@ -37,7 +37,7 @@ namespace Wangluodaxue2Motibang {
         }
         static void Main(string[] args) {
             string Help = @"
-备份：
+使用方法:
 w2m 全部/初级工/中级工/高级工/技师 d:/the/path/to/网络大学题库.xlsx d:/the/path/to/磨题帮题库.xlsx
 ";
             if (args.Length != 3) {
